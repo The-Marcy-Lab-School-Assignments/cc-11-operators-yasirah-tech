16f4f8fd94b222384ad0376b79c150b5ab487d6a
Yasirah <yasirahboyce1@gmail.com>
2021-11-02 14:51:08 +0000

    cc-11

--- index.js
+++ index.js
@@ -1,27 +1,61 @@
 //1
-function areBothSame(){
-  
+function areBothSame(value1, value2){
+  if (value1 == value2) {
+    return true;
+  } else {
+    return false;          
+  }
 }
+console.log(areBothSame(10, 6))
+
 
 //2
-function areBothEqual(){
-  
+function areBothEqual(value1, value2){
+  if (value1 === value2) {
+    return true;
+  } else {
+    return false;          
+  }
 }
+console.log(areBothSame(10, "6"))
+
+// function areBothEqual(value1, value2){
+// return (value1 === value2 ? true : false);
+
 
 //3
-function areAllFourEqual(){
-  
+  function areAllFourEqual(value1, value2, value3, value4){
+  if (value1 === value2 && value1 === value3 && value1 === value4 ) {
+    return true;
+  } else {
+    return false;          
+  }
 }
+console.log(areAllFourEqual(10, 5, 10,10))
+
 
 
 //4
-function areAllFourSame(){
-  
+  function areAllFourSame(value1, value2, value3, value4){
+  if (value1 == value2 && value1 == value3 && value1 == value4 ) {
+    return true;
+  } else {
+    return false;          
+  }
 }
+console.log(areAllFourSame(10, "10", 10,10))
+
 
 //5
-function areAllFourSameOrEqual(){
-  
+ function areAllFourSameOrEqual(value1, value2, value3, value4){
+  if (value1 == value2 && value1 == value3 && value1 === value4 || value1 === value2 && value1 === value3 && value1 === value4 ) {
+    return true;
+  } else {
+    return false;          
+  }
 }
+console.log(areAllFourSame(10, "10", 10,10))
+ 
+
 
 
